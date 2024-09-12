0143a3712d01d502b94cc8c648c60042feee1c44
Sunil Patel <sunilpatel12221@gmail.com>
2024-09-12 18:16:33 +0530

    Branch master completed

--- CIT.API/Controllers/BranchController.cs
+++ CIT.API/Controllers/BranchController.cs
@@ -1,7 +1,12 @@
-﻿using CIT.API.Models;
+﻿using AutoMapper;
+using CIT.API.Models;
 using CIT.API.Models.Dto;
+using CIT.API.Models.Dto.Customer;
 using CIT.API.Repository.IRepository;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
 
 namespace CIT.API.Controllers
 {
@@ -11,58 +16,74 @@ namespace CIT.API.Controllers
     {
         public readonly IBranchRepositoty _Ibranchrepositoty;
         protected APIResponse _response;
-        public BranchController(IBranchRepositoty branchRepositoty)
+        private readonly IMapper _mapper;
+        public BranchController(IBranchRepositoty branchRepositoty, IMapper mapper)
         {
             _Ibranchrepositoty = branchRepositoty;
+            _mapper = mapper;
+            _response = new();
         }
 
         [HttpGet("GetAllBranchAPI")]
-        public async Task<IActionResult> GetAllbranch()
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetAllbranch()
         {
             try
             {
-                IEnumerable<BranchMaster> branchModels;
+                IEnumerable<BranchMaster> branchModels = await _Ibranchrepositoty.GetAllBranch();
 
-                branchModels = await _Ibranchrepositoty.GetAllBranch();
-
-                return Ok(branchModels);
-            }
-
-            catch (Exception ex)
-            {
-                return Problem(ex.Message, ex.StackTrace);
-            }
-        }
-
-        [HttpPost("AddbranchAPI")]
-        public async Task<IActionResult> Addbranch(BranchDTO branchDTO)
-        {
-            int Res = 0;
-            try
-            {
-                if (ModelState.IsValid)
+                if (branchModels == null || !branchModels.Any())
                 {
-
-                    Res = await _Ibranchrepositoty.AddBranch(branchDTO);
-
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("No Branch found.");
+                    return NotFound(_response);
                 }
-                return Ok(Res);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = _mapper.Map<List<BranchDTO>>(branchModels);
+                return Ok(_response);
             }
+
             catch (Exception ex)
             {
-                return Problem(ex.Message, ex.StackTrace);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
             }
-
         }
 
-        [HttpGet("GetBranchAPI")]
-        public async Task<IActionResult> GetBranch([FromRoute(Name = "branchId")] int branchId)
+        //[HttpGet("GetBranchAPI")]
+        [HttpGet("{branchId:int}", Name = "GetBranch")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBranch(int branchId)
         {
             BranchMaster branchModel = new BranchMaster();
             try
             {
+                if (branchId == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
                 branchModel = await _Ibranchrepositoty.GetBranch(branchId);
-                return Ok(branchModel);
+
+                if (branchModel == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    return NotFound(_response);
+                }
+                _response.Result = _mapper.Map<BranchDTO>(branchModel);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
             }
             catch (Exception ex)
             {
@@ -71,36 +92,121 @@ namespace CIT.API.Controllers
 
         }
 
-        [HttpPut("UpdateBranchAPI")]
-        public async Task<IActionResult> UpdateBranch(BranchDTO branchRequestModel)
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> Addbranch([FromBody] BranchDTO branchDTO)
         {
             int Res = 0;
             try
             {
-                Res = await _Ibranchrepositoty.UpdateBranch(branchRequestModel);
-                return Ok(Res);
+                if (branchDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Invalid Branch data");
+                    return BadRequest(_response);
+                }
+
+                Res = await _Ibranchrepositoty.AddBranch(branchDTO);
+                if (Res == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Branch already exists.");
+                    return BadRequest(_response);
+                }
+                if (Res > 0)
+                {
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
             }
             catch (Exception ex)
             {
-
                 return Problem(ex.Message, ex.StackTrace);
             }
+            return _response;
         }
 
-        [HttpDelete("DeleteBranchAPI")]
-        public async Task<IActionResult> DeleteBranch(int branchId, int deletedBy)
+        [HttpPut("{BranchID:int}", Name = "UpdateBranch")]
+        //[Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateBranch(int BranchID, BranchDTO branchRequestModel)
         {
             int Res = 0;
             try
             {
+                if (branchRequestModel == null || BranchID != branchRequestModel.BranchID)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Invalid customer Id.");
+
+                    return BadRequest(_response);
+                }
+                BranchMaster branch = _mapper.Map<BranchMaster>(branchRequestModel);
+                Res = await _Ibranchrepositoty.UpdateBranch(branch);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("{branchId:int}", Name = "DeleteBranch")]
+        //[Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> DeleteBranch(int branchId, int deletedBy)
+        {
+            int Res = 0;
+            try
+            {
+
+                if (branchId == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Invalid Branch Id.");
+                    return BadRequest(_response);
+                }
+
                 Res = await _Ibranchrepositoty.DeleteBranch(branchId, deletedBy);
-                return Ok("The user is Deleted");
+                if (Res == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Branch not found.");
+                    return NotFound(_response);
+                }
+                if (Res > 0)
+                {
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
             }
             catch (Exception ex)
             {
-                return Problem(ex.Message, ex.StackTrace);
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
 
             }
+
+            return _response;
         }
     }
 }
--- CIT.API/Models/Dto/BranchDTO.cs
+++ CIT.API/Models/Dto/BranchDTO.cs
@@ -1,16 +1,17 @@
-﻿namespace CIT.API.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIT.API.Models.Dto
 {
     public class BranchDTO
     {
         public int? BranchID { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string? BranchName { get; set; }
+        [Required]
         public string? Address { get; set; }
         public string? ContactNumber { get; set; }
         public string? DataSource { get; set; }
-        public bool? IsActive { get; set; }
-        public int? CreatedBy { get; set; }
-        public int? ModifiedBy { get; set; }
-        public int? DeletedBy { get; set; }
-        public bool? IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
     }
 }
