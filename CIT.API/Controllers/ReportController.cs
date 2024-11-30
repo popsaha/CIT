2369d9f0108be057aa5103f976bcfb47ee31e1d6
Snehashish Saha <saha.snehashish@gmail.com>
2024-11-30 16:04:30 +0530

    add billing module

---
+++ CIT.API/Controllers/ReportController.cs
@@ -0,0 +1,102 @@
+﻿using Azure;
+using CIT.API.Models.Dto.Branch;
+using CIT.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using AutoMapper;
+using CIT.API.Repository.IRepository;
+
+
+namespace CIT.API.Controllers
+{
+    public class ReportController : ControllerBase
+    {
+        public readonly IReport _IReport;
+        protected APIResponse _response;
+        private readonly IMapper _mapper;
+        private readonly ILogger<BranchController> _logger;
+
+        public ReportController(IReport report, IMapper mapper, ILogger<BranchController> logger)
+        {
+            _IReport = report;
+            _mapper = mapper;
+            _logger = logger;
+            _response = new();
+        }
+
+
+        [HttpGet("GetReportsData")]
+
+        public async Task<ActionResult<ReportDetails>> GetReportsData()
+        {
+            _logger.LogInformation("GetReportsData method called at {time}.", DateTime.UtcNow);
+            IEnumerable<ReportDetails> ReportModels;
+
+            try
+            {
+                 ReportModels = await _IReport.GetAllReportData();
+
+
+                //if (ReportModels == null || !ReportModels.Any())
+                //{
+                //    _response.StatusCode = HttpStatusCode.NotFound;
+                //    _response.IsSuccess = false;
+                //    _response.ErrorMessages.Add("No Data found.");
+                //    return NotFound(_response);
+                //}
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = _mapper.Map<List<ReportDetails>>(ReportModels);
+
+                return Ok(_response);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving Reports at {time}.", DateTime.UtcNow);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
+
+           // return ReportModels;
+        }
+
+        [HttpPost("GetFilterReportsData")]
+        public async Task<ActionResult<ReportDetails>> GetFilterReportsData([FromBody] ReportDetailsParam _ReportDetailsParam)
+        {
+            _logger.LogInformation("GetFilterReportsData method called at {time}.", DateTime.UtcNow);
+            IEnumerable<ReportDetails> ReportModels;
+
+            try
+            {
+                ReportModels = await _IReport.GetFilteredReportData(_ReportDetailsParam);
+                //if (ReportModels == null || !ReportModels.Any())
+                //{
+                //    _response.StatusCode = HttpStatusCode.NotFound;
+                //    _response.IsSuccess = false;
+                //    _response.ErrorMessages.Add("No Data found.");
+                //    return NotFound(_response);
+                //}
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = _mapper.Map<List<ReportDetails>>(ReportModels);
+
+                return Ok(_response);
+
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving GetFilterReportsData() at {time}.", DateTime.UtcNow);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
+
+           //return ReportModels;
+        }
+    }
+}
---
+++ CIT.API/Models/ReportDetails.cs
@@ -0,0 +1,21 @@
+﻿namespace CIT.API.Models
+{
+    public class ReportDetails
+    {
+        public string CustomerName { get; set; }
+        public string BranchName { get; set; }
+        public string PickupTypeName { get; set; }
+        public string DistanceKM { get; set; }
+        public string Trip { get; set; }
+        public string Bill { get; set; }
+    }
+
+    public class ReportDetailsParam
+    {
+        public string Customerid { get; set; }
+        public string Branchid { get; set; }
+        public string PickupTypeid { get; set; }
+        public string fromDate { get; set; }
+        public string ToDate { get; set; }
+    }
+}
